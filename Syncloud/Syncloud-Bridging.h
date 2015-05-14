5c1cfd92ff44bfe994fe69f57eeb12aab4407039
v.sapronov <vladimir.sapronov@gmail.com>
2015-05-13 20:35:10 -0400

    Added NMSSH Pod

---
+++ Syncloud/Syncloud-Bridging.h
@@ -0,0 +1,15 @@
+//
+//  NMSSH-Bridging.h
+//  Syncloud
+//
+//  Created by Vladimir Sapronov on 5/13/15.
+//  Copyright (c) 2015 Syncloud. All rights reserved.
+//
+
+#ifndef Syncloud_NMSSH_Bridging_h
+#define Syncloud_NMSSH_Bridging_h
+
+#import <Foundation/Foundation.h>
+#import <NMSSH/NMSSH.h>
+
+#endif
